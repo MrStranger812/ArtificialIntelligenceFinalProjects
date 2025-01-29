6c755ef2995a5ac559e0f07dd52958e3d4b5d09e
sina <mrstranger812@gmail.com>
2025-01-29 11:57:21 +0330

    Add project files.

---
+++ wwwroot/timeChart.js
@@ -0,0 +1,32 @@
+﻿console.log("timeChart.js loaded!");
+window.timeChartFunctions = {
+    chartInstance: null,
+
+    updateChart: function (canvasId, labels, data) {
+        if (!this.chartInstance) {
+            // Create new chart
+            const ctx = document.getElementById(canvasId).getContext('2d');
+            this.chartInstance = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Algorithm Time (ms)',
+                        data: data,
+                        backgroundColor: ['rgba(54, 162, 235, 0.6)', 'rgba(255, 99, 132, 0.6)'],
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: { beginAtZero: true }
+                    }
+                }
+            });
+        } else {
+            // Update existing chart
+            this.chartInstance.data.labels = labels;
+            this.chartInstance.data.datasets[0].data = data;
+            this.chartInstance.update();
+        }
+    }
+};
